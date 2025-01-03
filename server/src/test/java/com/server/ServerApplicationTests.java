c93abba99163c757c5ffdddbed066a1d8cad9b0e
DESKTOP-LSGC5E0\Darek <darko1601@gmail.com>
2025-01-03 19:00:52 +0100

    Fix after test #5

--- server/src/test/java/com/server/ServerApplicationTests.java
+++ server/src/test/java/com/server/ServerApplicationTests.java
@@ -3,11 +3,20 @@ package com.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void sampleTest() {
+		int expected = 2137;
+		int actual = 2135 + 1;
+		assertThat(actual).isEqualTo(expected);
 	}
 
 }
